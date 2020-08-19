65fa7b902d75538ffc012e06edd7e242f8524ebc
hersson ceballos <herssonceballos@gmail.com>
2020-08-19 21:32:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,21 +3,8 @@ class Dog
   def bark
     puts "Woof!"
   end
-end
-
-class Dog 
   
   def sit 
     puts "The Dog is sitting"
     end
-  end
-
-# class Dog
-#   def bark
-#     puts "Woof!"
-#   end
-# end
- 
-# fido = Dog.new
-# fido.bark #> "Woof!"
-# fido.sit # NoMethodError: undefined method `sit' for #<Dog:0x007fa4e9a9e8a0>
+end 
